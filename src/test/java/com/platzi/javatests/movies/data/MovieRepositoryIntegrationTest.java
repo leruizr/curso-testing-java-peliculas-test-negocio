f3475709f21f8a24f183b5473a5b305cfefec87f
leruizr <leruizres1986@gmail.com>
2024-08-09 11:46:57 -0500

    first commit

---
+++ src/test/java/com/platzi/javatests/movies/data/MovieRepositoryIntegrationTest.java
@@ -0,0 +1,68 @@
+package com.platzi.javatests.movies.data;
+
+import com.platzi.javatests.movies.model.Genre;
+import com.platzi.javatests.movies.model.Movie;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class MovieRepositoryIntegrationTest  {
+
+    private MovieRepositoryJdbc movieRepository;
+    private DriverManagerDataSource dataSource;
+
+    @Before
+    public void setUp() throws Exception {
+
+        dataSource =
+                new DriverManagerDataSource("jdbc:h2:mem:test;MODE=MYSQL", "sa", "sa");
+
+        ScriptUtils.executeSqlScript(dataSource.getConnection(), new ClassPathResource("sql-scripts/test-data.sql"));
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        movieRepository = new MovieRepositoryJdbc(jdbcTemplate);
+
+    }
+
+    @Test
+    public void load_all_movies() throws SQLException {
+
+        Collection<Movie> movies = movieRepository.findAll();
+
+        assertThat(movies, is(Arrays.asList(
+                new Movie(1, "Dark Knight", 152, Genre.ACTION),
+                new Movie(2, "Memento", 113, Genre.THRILLER),
+                new Movie(3, "Matrix", 136, Genre.ACTION)
+        )));
+    }
+
+    @Test
+    public void load_movie_by_id() throws SQLException {
+
+        Movie movie = movieRepository.findById(2);
+
+        assertThat(movie, is(new Movie(2, "Memento", 113, Genre.THRILLER)));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        // Clean up the test data
+       final Statement s = dataSource.getConnection().createStatement();
+       s.execute("drop all objects delete files");
+    }
+
+
+
+}
